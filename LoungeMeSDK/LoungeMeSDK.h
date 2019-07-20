2c7d4cb5ebe1614b2e6b9da4513228b2c39f8781
Doğu Emre DEMİRÇİVİ <me@emredemircivi.com>
2019-07-20 15:18:26 +0300

    Initial Commit

---
+++ LoungeMeSDK/LoungeMeSDK.h
@@ -0,0 +1,19 @@
+//
+//  LoungeMeSDK.h
+//  LoungeMeSDK
+//
+//  Created by Doğu Emre DEMİRÇİVİ on 20.07.2019.
+//  Copyright © 2019 TAV Airports. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoungeMeSDK.
+FOUNDATION_EXPORT double LoungeMeSDKVersionNumber;
+
+//! Project version string for LoungeMeSDK.
+FOUNDATION_EXPORT const unsigned char LoungeMeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoungeMeSDK/PublicHeader.h>
+
+
